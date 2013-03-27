fb008b37a0c2da271d1ee4679c1fc896a8a44c77
tianweidut <liutianweidlut@qq.com>
2013-03-27 10:21:23 +0900

    Add south migration files 1. school model 2. adminStaff model 3. users model 4. registration model 5. news model 6. expert model 7. const model

---
+++ news/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'News'
+        db.create_table('news_news', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('news_title', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('news_content', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('news_date', self.gf('django.db.models.fields.DateField')(default=datetime.datetime(2013, 3, 26, 0, 0), blank=True)),
+        ))
+        db.send_create_signal('news', ['News'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'News'
+        db.delete_table('news_news')
+
+
+    models = {
+        'news.news': {
+            'Meta': {'object_name': 'News'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'news_content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'news_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 3, 26, 0, 0)', 'blank': 'True'}),
+            'news_title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['news']
