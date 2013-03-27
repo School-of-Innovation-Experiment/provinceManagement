0f8cea9459c020ba942e42e671bf07da733fa904
tianweidut <liutianweidlut@qq.com>
2013-03-27 01:01:37 +0000

    add adminstaff models

--- adminStaff/models.py
+++ adminStaff/models.py
@@ -1,3 +1,57 @@
+# coding: UTF-8
+'''
+Created on 2013-03-28
+
+@author: tianwei
+
+Desc: Province Admin staff
+'''
+
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+from const.models import *
+from school.models import *
+
+
+class ProjectControl(models.Model):
+    """
+    Project date control
+    """
+    pre_start_day = models.DateField(blank=False,
+                                     verbose_name="申请报告提交开始时间")
+    pre_end_day = models.DateField(blank=False,
+                                   verbose_name="申请报告提交结束时间")
+    pre_start_day_review = models.DateField(blank=False,
+                                            verbose_name="项目初审开始时间")
+    pre_end_day_review = models.DateField(blank=False,
+                                          verbose_name="项目初审结束时间")
+    final_start_day = models.DateField(blank=False,
+                                       verbose_name="结题报告提交开始时间")
+    final_end_day = models.DateField(blank=False,
+                                     verbose_name="结题报告提交结束时间")
+    final_start_day_review = models.DateField(blank=False,
+                                              verbose_name="项目终审开始时间")
+    final_end_day_review = models.DateField(blank=False,
+                                            verbose_name="项目终审结束时间")
+
+    class Meta:
+        verbose_name = "时间节点控制"
+        verbose_name_plural = "时间节点控制"
+
+
+class ReviewTask(models.Model):
+    review_id = models.CharField(max_length=50, blank=False, unique=True,
+                                 primary_key=True, default=uuid4(),
+                                 verbose_name="题目唯一ID")
+    project_id = models.ForeignKey(ProjectSingle)
+    comments = models.TextField(blank=False, verbose_name="评价")
+    scores = models.IntegerField(blank=False, verbose_name="评分百分制")
+
+    class Meta:
+        verbose_name = "评审任务"
+        verbose_name_plural = "评审任务"
+
+    def __unicode__(self):
+        return self.project_id.title
--- users/models.py
+++ users/models.py
@@ -9,27 +9,56 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-DEFAULT_CREATE_ID = "0000-0000"
-DEFAULT_ERROR_ID = "FFFF-FFFF"
+from const.models import *
 
-class UserProfile(models.Model):
+
+class SchoolProfile(models.Model):
     """
     User Profile Extend
     The Administrator can modified them in admin.page
     """
     user = models.OneToOneField(User)
-    machinecode = models.CharField(max_length = 100)
-    agentID = models.CharField(max_length = 40,default = uuid.uuid4(),unique=True)         #When the userProfile is created,agentId will be created automatically.
-    workunit = models.CharField(max_length = 2000,blank=True)
-    address  = models.CharField(max_length = 2000,blank=True)
-    telephone = models.CharField(max_length = 100, blank=True)
-    
+    school = models.ForeignKey(SchoolDict)
+    identity = models.ForeignKey(UserIdentity)
+
+    class Meta:
+        verbose_name = "参赛学校"
+        verbose_name_plural = "参赛学校"
+
     def __unicode__(self):
-        return '%s' %(self.user)
-    
+        return '%s' % (self.user)
+
+
+class ExperterProfile(models.Model):
+    user = models.OneToOneField(User)
+    subject = models.FileField(InsituteCategory)
+    identity = models.ForeignKey(UserIdentity)
+
+    class Meta:
+        verbose_name = "评审专家"
+        verbose_name_plural = "评审专家"
+
+    def __unicode__(self):
+        return '%s' % (self.user)
+
+
+class AdminStaffProfile(models.Model):
+    user = models.OneToOneField(User)
+    identity = models.ForeignKey(UserIdentity)
+
+    class Meta:
+        verbose_name = "省级管理员"
+        verbose_name_plural = "省级管理员"
+
+    def __unicode__(self):
+        return '%s' % (self.user)
+
+
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        SchoolProfile.objects.create(user=instance)
+        ExperterProfile.objects.create(user=instance)
+        AdminStaffProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
     
