5d96ed849cac786343c9c75b589fb130ffcb3abf
tianweidut <liutianweidlut@qq.com>
2013-03-27 01:25:53 +0000

    re-add UserProfile into code for debug

--- users/models.py
+++ users/models.py
@@ -12,6 +12,22 @@ from django.db.models.signals import post_save
 from const.models import *
 
 
+class UserProfile(models.Model):
+    """
+    User Profile Extend
+    The Administrator can modified them in admin.page
+    """
+    user = models.OneToOneField(User)
+    machinecode = models.CharField(max_length = 100)
+    agentID = models.CharField(max_length = 40,default = uuid.uuid4(),unique=True)         #When the userProfile is created,agentId will be created automatically.
+    workunit = models.CharField(max_length = 2000,blank=True)
+    address  = models.CharField(max_length = 2000,blank=True)
+    telephone = models.CharField(max_length = 100, blank=True)
+    
+    def __unicode__(self):
+        return '%s' %(self.user)
+    
+
 class SchoolProfile(models.Model):
     """
     User Profile Extend
@@ -31,8 +47,9 @@ class SchoolProfile(models.Model):
 
 class ExperterProfile(models.Model):
     user = models.OneToOneField(User)
-    subject = models.FileField(InsituteCategory)
+    subject = models.ForeignKey(InsituteCategory)
     identity = models.ForeignKey(UserIdentity)
+    workunit = models.CharField(max_length=100, blank=True)
 
     class Meta:
         verbose_name = "评审专家"
@@ -59,6 +76,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         SchoolProfile.objects.create(user=instance)
         ExperterProfile.objects.create(user=instance)
         AdminStaffProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
     
