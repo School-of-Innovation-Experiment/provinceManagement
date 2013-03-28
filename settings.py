9d72cab87e448c16aed2d32193ae9cc0eb286fd1
LiaoPengyu <ariselpy@gmail.com>
2013-03-28 18:06:31 +0800

    add media-root to static-dirs

--- settings.py
+++ settings.py
@@ -77,6 +77,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     COMMON_STITICFILES_DIR,
+    MEDIA_ROOT,
 )
 
 # List of finder classes that know how to find static files in
