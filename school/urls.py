9b903d436e55e0f499ef78c58682f14b8f76da87
tianweidut <liutianweidlut@qq.com>
2013-03-28 11:18:07 +0900

    build the basic URL map for school view

--- school/urls.py
+++ school/urls.py
@@ -1,58 +1,34 @@
-# -*- coding: UTF-8 -*-
+# coding: UTF-8
 '''
-Created on 2013-3-11
+Created on 2013-3-28
 
-@author: yaoyuan
+@author: tianwei
+
+Desc: School URL defination
 '''
 
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('',
+from school import views as school_views
 
+
+urlpatterns = patterns('',
     url(
         r'^$',
-        direct_to_template, {'template': 'school/home.html'}
+        school_views.home_view,
     ),
     url(
-        r'^initial$',
-        direct_to_template, {'template': 'school/initial.html'}
-    ),
-        url(
-        r'^initial1$',
-        direct_to_template, {'template': 'school/initial1.html'}
+        r'^(?P<id>\w+)/final$',
+        school_views.final_report_view,
     ),
     url(
-        r'^midterm$',
-        direct_to_template, {'template': 'school/midterm.html'}
+        r'^(?P<id>\w+)/application$',
+        school_views.application_report_view,
     ),
     url(
-        r'^final$',
-        direct_to_template, {'template': 'school/final.html'}
-    ),
-    url(
-        r'^progress$',
-        direct_to_template, {'template': 'school/progress.html'}
-    ),
-    url(
-        r'^finance$',
-        direct_to_template, {'template': 'school/finance.html'}
-    ),
-    url(
-        r'^studentcharge$',
-        direct_to_template, {'template': 'school/studentcharge.html'}
-    ),
-        url(
-        r'^modifyproject$',
-        direct_to_template, {'template': 'school/modifyproject.html'}
-    ),
-            url(
-        r'^projectapp$',
-        direct_to_template, {'template': 'school/projectapp.html'}
-    ),
-            url(
-        r'^application$',
-        direct_to_template, {'template': 'school/application.html'}
+        r'^statistics/$',
+        school_views.statistics_view,
     ),
 )
--- school/views.py
+++ school/views.py
@@ -1,1 +1,79 @@
-# Create your views here.
+# coding: UTF-8
+'''
+Created on 2013-03-28
+
+@author: tianwei
+
+Desc: School's view, includes home(manage), final report,
+      application report, statistics information.
+'''
+import datetime
+import logging
+import os
+import sys
+
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseForbidden, Http404
+from django.template import RequestContext
+from django.utils import simplejson
+from django.views.decorators import csrf
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+
+
+#TODO: for decorators, later I will add time control, authority control
+
+@csrf.csrf_protect
+@login_required
+def home_view(request):
+    """
+    school home management page
+    """
+    data = {}
+
+    return render(request, 'school/home.html', data)
+
+
+@csrf.csrf_protect
+@login_required
+def application_report_view(request, id=None):
+    """
+    school application report
+    Arguments:
+        In: id, it is project id
+    """
+
+    data = {}
+
+    return render(request, 'school/application.html', data)
+
+
+@csrf.csrf_protect
+@login_required
+def final_report_view(request, id=None):
+    """
+    school final report
+    Arguments:
+        In: id, it is project id
+    """
+
+    data = {}
+
+    return render(request, 'school/final.html', data)
+
+
+@csrf.csrf_protect
+@login_required
+def statistics_view(request):
+    """
+    school statistics view
+    """
+
+    data = {}
+
+    return render(request, 'school/statistics.html', data)
