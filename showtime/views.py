1fa4c9d39a15739c278c0d093efbaf74f847c688
JunHu <hujun09012@gmail.com>
2013-04-03 22:07:31 +0800

    showtime introduction search function#need to fix some bug

--- showtime/views.py
+++ showtime/views.py
@@ -7,18 +7,37 @@ from django.db.models import Q
 from django.shortcuts import render
 from backend.utility import convert2media_url, getContext
 from const import PROJECT_GRADE_CHOICES, GRADE_NATION, DEFAULT_IMG_URL
+from const.models import SchoolDict, ProjectGrade
 
 GRADE_DICT = dict(PROJECT_GRADE_CHOICES)
+
 def show_index(request):
     # nation_project = ProjectGrade.objects.get(grade=GRADE_NATION)
     # return render(request, 'introduction/show.html', {})
     project_page = request.GET.get('project_page')
+
+    search_school = request.GET.get('search_school')
+    search_year = request.GET.get('search_year')
+    search_grade = request.GET.get('search_grade')
+
     try:
         project_page = int(project_page)
     except:
         project_page = 1
-    project_list = ProjectSingle.objects.all()
-    #filter( \project_grade=nation_project)
+    
+    q1 = (search_year and Q(year=search_year)) or None
+    q2 = (search_school and Q(school=search_school)) or None
+    q3 = (search_grade and Q(project_grade=search_grade)) or None
+    qset = filter(lambda x: x != None, [q1, q2, q3])
+    if qset :
+        qset = reduce(lambda x, y: x & y, qset) 
+        project_list = ProjectSingle.objects.all().filter(qset)
+    else:
+        project_list = ProjectSingle.objects.all()
+    # project_list = ProjectSingle.objects.all()
+				#Q(project_year=nation_project))
+	
+
     context = getContext(project_list, project_page, 'project', 8)
     if project_page != context["project_page"].number:
         raise Http404
@@ -28,4 +47,15 @@ def show_index(request):
             Q(file_obj__iendswith="png") )
         project.img = (imgs.count() and convert2media_url(imgs[0].file_obj.url)) or \
             DEFAULT_IMG_URL
+
+	school_list = SchoolDict.objects.all()
+	grade_list = ProjectGrade.objects.all()
+
+	context["schools"] = school_list
+	context["grades"] = grade_list
+
+	context["search_school"] = search_school
+	context["search_year"] = search_year
+	context["search_grade"] = search_grade
+
     return render(request, 'introduction/show.html', context)
