c22caa6df5cf9ad89873993c01268444edd150de
LiaoPengyu <ariselpy@gmail.com>
2013-04-10 15:24:57 +0800

    js for notice message

--- media/js/adminstaff/noticemessage.js
+++ media/js/adminstaff/noticemessage.js
@@ -1,7 +1,28 @@
-$("#noticemessage_form").onsubmit = function (){
-  if($("#select_role").get(0).selectedIndex == 0)
-  {
-    $("#noticemessage_warning");
-    return false;
-  }
-}
+$(function(){
+  var form = $("#noticemessage_form");
+  var select = $("#select_role");
+  var error = $("#noticemessage_warning");
+  form.submit(
+    function (){
+      if(select.get(0).selectedIndex == 0)
+      {
+        error.css("display","");
+        select.css("border-color", "#b94a48");
+        return false;
+      }
+      return true;
+    });
+  select.change(
+    function (){
+      if(select.get(0).selectedIndex == 0)
+        {
+          select.css("border-color", "#b94a48");
+        }
+      else
+      {
+        select.css("border-color", "#cccccc");
+      }
+    }
+  );
+});
+
