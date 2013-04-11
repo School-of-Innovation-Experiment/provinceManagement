491d5de1c03681a8343f82739fe86c686909ae91
shenlian <1019947054@qq.com>
2013-04-11 12:46:31 +0800

    update checkbrowser css

--- media/js/home/checkbrowser.js
+++ media/js/home/checkbrowser.js
@@ -2,9 +2,12 @@ function getOs()
 { 
 	var checkerror=false;
 	if(isSafari=navigator.userAgent.indexOf("Safari")>0) { 
-		checkerror=true;
+        checkerror=true;
 	} 
     if(isFirefox=navigator.userAgent.indexOf("Firefox")>0){  
+        checkerror=true;
+    } 
+    if(window.opera){  
         checkerror=true;
     } 
 	if (checkerror==false) {
