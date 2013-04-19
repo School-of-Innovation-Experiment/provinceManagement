4b0eff72ba0f3ae8b1696a9430ea9b010ffef5e8
LiaoPengyu <ariselpy@gmail.com>
2013-04-19 14:07:03 +0800

    create teacherlimits when teacherprofile create in send-email

--- media/js/school/send_num_limit.js
+++ media/js/school/send_num_limit.js
@@ -1,11 +1,13 @@
 function send_num_limit(){
   $("#num_limit_error_message").empty();
-  Dajaxice.school.NumLimit(limitednum_callback,{'form':$('#num_limit_form').serialize(true)});
+  Dajaxice.school.teacherProjNumLimit(limitednum_callback,
+                                      {'form':$('#num_limit_form').serialize(true)});
 }
 function limitednum_callback(data){
-  if (data.status == "1"){
+  if (data.status == "1"){    // sucess
     $("#limited_num").css("background","white");
     $("#num_limit_error_message").append("<strong>"+data.message+"</strong>");
+    $("#projects_remaining").text('剩余数量:'+data["projects_remaining"]);
   }
   else
   {
