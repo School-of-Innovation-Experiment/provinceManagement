e5b07e644124350ffc4f8856db4f9c5cc3ddb882
tianweidut <liutianweidlut@qq.com>
2013-05-20 16:23:50 +0900

    promove the delete file process

--- media/js/school/delete.js
+++ media/js/school/delete.js
@@ -1,5 +1,9 @@
 /* file delete operation*/
 
+$(document).ready(function(){
+  $("div#delete-error-panel").hide();
+});
+
 $('[rel="file_delete"]').click(function(){
   
   var pid = $(this).attr("pid");
@@ -10,11 +14,17 @@ $('[rel="file_delete"]').click(function(){
 
 function file_delete_callback(data){
   if(data.is_deleted === true){
+      var fid = "tr[id=" + data.fid +"]";
       console.log("successs!");
       console.log(data.message);
+      console.log(fid);
+      $(fid).remove();
     }
   else{
       console.log("Failed!");
       console.log(data.message);
+      $("div#delete-error-panel").show();
+      $("p#delete-message").text(data.message);
+
   }
 }
