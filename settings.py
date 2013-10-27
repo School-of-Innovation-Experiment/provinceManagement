18f9a7ef628313d6575e6436428366f848e3ad3b
DutBright <757289247@qq.com>
2013-10-28 01:15:46 +0800

    change mail server

--- settings.py
+++ settings.py
@@ -156,25 +156,25 @@ INSTALLED_APPS = (
 ACCOUNT_ACTIVATION_DAYS = 30
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-SERVER_EMAIL = "dut200921049@gmail.com"
-EMAIL_SUBJECT_PREFIX = '[ProvinceInnovationManagement]'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'dut200921049@gmail.com'
-EMAIL_HOST_PASSWORD = '09031632'
-DEFAULT_FROM_EMAIL = 'dut200921049@gmail.com'
-EMAIL_USE_TLS = False
-
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# SERVER_EMAIL = "tianweidut@mail.dlut.edu.cn"
-# EMAIL_SUBJECT_PREFIX = '[MinzuInnovationManagement]'
-# EMAIL_HOST = 'mail.dlut.edu.cn'
-# EMAIL_PORT = '25'
-# EMAIL_HOST_USER = 'tianweidut@mail.dlut.edu.cn'
-# EMAIL_HOST_PASSWORD = '9683096830'
-# DEFAULT_FROM_EMAIL = 'tianweidut@mail.dlut.edu.cn'
+# SERVER_EMAIL = "dut200921049@gmail.com"
+# EMAIL_SUBJECT_PREFIX = '[ProvinceInnovationManagement]'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'dut200921049@gmail.com'
+# EMAIL_HOST_PASSWORD = '09031632'
+# DEFAULT_FROM_EMAIL = 'dut200921049@gmail.com'
 # EMAIL_USE_TLS = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SERVER_EMAIL = "tianweidut@mail.dlut.edu.cn"
+EMAIL_SUBJECT_PREFIX = '[MinzuInnovationManagement]'
+EMAIL_HOST = 'mail.dlut.edu.cn'
+EMAIL_PORT = '25'
+EMAIL_HOST_USER = 'tianweidut@mail.dlut.edu.cn'
+EMAIL_HOST_PASSWORD = '9683096830'
+DEFAULT_FROM_EMAIL = 'tianweidut@mail.dlut.edu.cn'
+EMAIL_USE_TLS = False
 
 
 #########################
