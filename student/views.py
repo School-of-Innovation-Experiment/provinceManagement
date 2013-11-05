b993ee257e14b0fe6e22f0aa023bd404072c8f8f
DutBright <757289247@qq.com>
2013-11-05 20:56:13 +0800

    delete loginfo

--- student/views.py
+++ student/views.py
@@ -202,7 +202,6 @@ def final_report_view(request, pid=None,is_expired=False):
     # techcompetition=get_object_or_404(TechCompetition,project_id=final.content_id)
     is_finishing = check_finishingyear(project)
     over_status = project.over_status
-    loginfo(p=over_status.status, label="STATUS")
     try:
         readonly = (over_status.status != OVER_STATUS_NOTOVER) or not is_finishing
     except:
