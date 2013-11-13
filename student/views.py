581bc959710a2a5f314d3cf48e84f34e510214c8
JunHu <hujun09012@gmail.com>
2013-11-13 21:07:22 +0800

    fix bug multi-identity error in subject_alloc page

--- student/views.py
+++ student/views.py
@@ -54,6 +54,7 @@ def member_change(request):
     """
     
     student_account = StudentProfile.objects.get(userid = request.user)
+    print student_account
     project = ProjectSingle.objects.get(student=student_account)
     student_group = Student_Group.objects.filter(project = project)
 
