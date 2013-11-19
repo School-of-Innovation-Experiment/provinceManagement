95b806bd7b700d0bca4616137f8fa67449e7cbdc
LiaoPengyu <ariselpy@gmail.com>
2013-11-19 23:34:50 +0800

    fix excel export alert

--- media/js/adminstaff/project_informationexport.js
+++ media/js/adminstaff/project_informationexport.js
@@ -3,12 +3,11 @@ function releaseexcel_baseinformation(){
   Dajaxice.adminStaff.Release_Excel(releaseexcel_baseinformation_callback,{'exceltype':1});
 }
 function releaseexcel_baseinformation_callback(data){
-	location.href = data.path
-	$('#excelprogress').modal('hide');
+  location.href = data.path;
+  $('#excelprogress').modal('hide');
 }
 
 function releaseexcel_expertscore(){
   $('#excelprogress').modal('show');
-  alert("haha");
   Dajaxice.adminStaff.Release_Excel(releaseexcel_baseinformation_callback,{'exceltype':2});
 }
