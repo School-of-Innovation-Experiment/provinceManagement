edfbe1fe1804d651c36b007379636ca9edb7cb8d
liuzhuocpp <liuzhuocpp@qq.com>
2014-01-06 15:27:17 +0800

    fix conflict

--- school/views.py
+++ school/views.py
@@ -48,6 +48,156 @@ from student.models import Funds_Group
 
 from settings import IS_MINZU_SCHOOL
 
+
+from school.forms import InfoForm, ApplicationReportForm, FinalReportForm,EnterpriseApplicationReportForm,TechCompetitionForm,Teacher_EnterpriseForm
+from student.forms import StudentGroupForm, StudentGroupInfoForm,ProcessRecordForm
+@csrf.csrf_protect
+@login_required
+@authority_required(SCHOOL_USER)
+def member_change(request, pid):
+        """
+        project group member change
+        """
+        #student_account = StudentProfile.objects.get(userid = request.user)
+        #project = ProjectSingle.objects.get(student=student_account)
+
+        project = ProjectSingle.objects.get(project_id = pid) 
+
+        student_group = Student_Group.objects.filter(project = project)
+
+        for s in student_group:
+            s.sex = s.get_sex_display()
+
+        student_group_form = StudentGroupForm()
+        student_group_info_form = StudentGroupInfoForm()
+        return render(request, "school/member_change.html",
+                      {"pid": pid,
+                       "student_group": student_group,
+                       "student_group_form": student_group_form,
+                       "student_group_info_form": student_group_info_form})
+
+
+@csrf.csrf_protect
+@login_required
+@authority_required(SCHOOL_USER)
+def final_report_view(request, pid=None):
+        """
+        student final report
+        Arguments:
+            In: id, it is project id
+        """
+        is_expired=False
+        # loginfo(p=pid+str(is_expired), label="in application")
+
+        final = get_object_or_404(FinalSubmit, project_id=pid)
+        project = get_object_or_404(ProjectSingle, project_id=pid)
+        #techcompetition=get_object_or_404(TechCompetition,project_id=final.content_id)
+        is_finishing = check_finishingyear(project)
+        over_status = project.over_status
+
+        readonly = (over_status != OVER_STATUS_NOTOVER) or not is_finishing
+
+        readonly = False
+        if request.method == "POST" and readonly is not True:
+            final_form = FinalReportForm(request.POST, instance=final)
+            # techcompetition_form =
+            if final_form.is_valid():
+                final_form.save()
+                project.project_status = ProjectStatus.objects.get(status=STATUS_FINSUBMIT)
+                project.save()
+                #return HttpResponseRedirect(reverse('student.views.home_view'))
+            else:
+                pass
+                # logger.info("Final Form Valid Failed"+"**"*10)
+                # logger.info(final_form.errors)
+                # logger.info("--"*10)
+
+        final_form = FinalReportForm(instance=final)
+        #techcompetition_form = TechCompetitionForm(instance=techcompetition)
+
+        data = {'pid': pid,
+                'final': final_form,
+              #   'techcompetition':techcompetition,
+                'readonly':readonly,
+                }
+        return render(request, 'school/final.html', data)
+
+
+@csrf.csrf_protect
+@login_required
+@authority_required(SCHOOL_USER)
+def application_report_view(request, pid=None):
+        
+        """
+            readonly determined by time
+            is_show determined by identity
+            is_innovation determined by project_category
+        """
+        is_expired=False
+        # loginfo(p=pid+str(is_expired), label="in application")
+        project = get_object_or_404(ProjectSingle, project_id=pid) 
+        is_currentyear = check_year(project)
+        is_applying = check_applycontrol(project)
+        #readonly= is_expired or (not is_currentyear) or (not is_applying)
+        readonly = False
+        is_show =  check_auth(user=request.user,authority=STUDENT_USER)
+        
+
+        if project.project_category.category == CATE_INNOVATION:
+            iform = ApplicationReportForm
+            pre = get_object_or_404(PreSubmit, project_id=pid)
+            teacher_enterprise=None
+            is_innovation = True
+        else:
+            iform = EnterpriseApplicationReportForm
+            pre = get_object_or_404(PreSubmitEnterprise, project_id=pid)
+            teacher_enterprise = get_object_or_404(Teacher_Enterprise,id=pre.enterpriseTeacher_id)
+            is_innovation = False
+
+        teacher_enterpriseform=Teacher_EnterpriseForm(instance=teacher_enterprise)
+        if request.method == "POST" and readonly is not True:
+            info_form = InfoForm(request.POST,pid=pid,instance=project)
+            application_form = iform(request.POST, instance=pre)
+            if is_innovation == True:
+                if info_form.is_valid() and application_form.is_valid():
+                    if save_application(project, pre, info_form, application_form, request.user):
+                        project.project_status = ProjectStatus.objects.get(status=STATUS_PRESUBMIT)
+                        project.save()
+                else:
+                    pass                    
+                    # logger.info(" info  application Form Valid Failed"+"**"*10)
+                    # logger.info(info_form.errors)
+                    # logger.info(application_form.errors)
+                    # logger.info("--"*10)
+            else :
+                teacher_enterpriseform=Teacher_EnterpriseForm(request.POST,instance=teacher_enterprise)
+                if info_form.is_valid() and application_form.is_valid() and teacher_enterpriseform.is_valid():
+                    if save_enterpriseapplication(project, pre, info_form, application_form, teacher_enterpriseform,request.user):
+                        project.project_status = ProjectStatus.objects.get(status=STATUS_PRESUBMIT)
+                        project.save()
+                else:
+                    pass
+                    # logger.info("info  application teacher Form Valid Failed"+"**"*10)
+                    # logger.info(info_form.errors)
+                    # logger.info(application_form.errors)
+                    # logger.info(teacher_enterpriseform.errors)
+                    # logger.info("--"*10)
+        else:
+            info_form = InfoForm(instance=project,pid=pid)
+            application_form = iform(instance=pre)
+            # teacher_enterpriseform=Teacher_EnterpriseForm(instance=teacher_enterprise)
+        data = {'pid': pid,
+                'info': info_form,
+                'application': application_form,
+                'teacher_enterpriseform':teacher_enterpriseform,
+                'readonly': readonly,
+                'is_innovation':is_innovation,
+                'is_show':is_show,
+                }
+        return render(request, 'school/application.html', data)
+
+
+
 @csrf.csrf_protect
 @login_required
 @authority_required(SCHOOL_USER)
