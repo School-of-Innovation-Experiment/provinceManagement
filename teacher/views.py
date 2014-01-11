ad4e0551e984cc2028bbdc62e8f8d88bfd48e978
DutBright <757289247@qq.com>
2014-01-11 15:36:06 +0800

    modify application write

--- teacher/views.py
+++ teacher/views.py
@@ -54,7 +54,7 @@ def application_report_view(request,pid=None,is_expired=False):
     is_currentyear = check_year(project)
     is_applying = check_applycontrol(project)  
     # readonly= is_expired or not is_currentyear or not is_applying
-    readonly=  not is_applying
+    readonly= not is_applying or project.is_past or not is_expired
     if check_auth(user=request.user,authority=TEACHER_USER):
         is_show = False
     else:
