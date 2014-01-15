5161791c146c875f3f7de3c54fd0a780d4c17010
shenlian <1019947054@qq.com>
2014-01-15 19:49:46 +0800

    merge projectsummary excel

--- media/js/adminstaff/project_informationexport.js
+++ media/js/adminstaff/project_informationexport.js
@@ -3,3 +3,12 @@ function releaseexcel_callback(data){
   $('#excelprogress').modal('hide');
 }
 
+function releaseexcel_expertscore(){
+  $('#excelprogress').modal('show');
+  Dajaxice.adminStaff.Release_Excel(releaseexcel_baseinformation_callback,{'exceltype':2});
+}
+
+function releaseexcel_projectsummary(){
+  $('#excelprogress').modal('show');
+  Dajaxice.adminStaff.Release_Excel(releaseexcel_baseinformation_callback,{'exceltype':3});
+}
