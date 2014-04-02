e7908ab0d6edcee37fbbd7f0709d393667056d41
DutBright <757289247@qq.com>
2014-04-02 11:25:02 +0800

    add the processrecord delete and modify

--- media/js/teacher/comment_change.js
+++ media/js/teacher/comment_change.js
@@ -1,13 +1,16 @@
 var pid;
-$(document).ready(function(){
+$().ready(function(){
   var strUrl  = location.href.split("/");
   pid     = strUrl[strUrl.length-1];
-  
+  $('#comment_table').on("click","div",function(){
+    $('#comment_info_form').find("input[name=monthId]").val($(this).children("h4").attr("val"));
+    $('#comment_info_form').find("textarea[name=commenttext]").val($(this).children("#comment_summary").text());
+  })
 })
 function add_or_update_comment() {
   $("#comment_error_message").empty();
   Dajaxice.teacher.commentChange(add_or_update_comment_callback,
-                                {'form':$('#process_record_info_form').serialize(true),
+                                {'form':$('#comment_info_form').serialize(true),
                                   'pid':pid
                                 });
 };
