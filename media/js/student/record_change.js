863387a7516098443bcbba47f58ffdfe4fb4b5a1
DutBright <757289247@qq.com>
2014-04-09 16:18:05 +0800

    add the taacher comment delete

--- media/js/student/record_change.js
+++ media/js/student/record_change.js
@@ -12,11 +12,11 @@ $().ready(function(){
                                   });
   };
   function add_or_update_record_callback(data) {
-      if(data.status == "2") {
-      $.each(data.error_id, function (i, item){
-        object = $('#'+item);
-        object.css("border-color", 'red');
-      });
+    if(data.status == "2") {
+       $.each(data.error_id, function (i, item){
+         object = $('#'+item);
+         object.css("border-color", 'red');
+       });
     }
     else if(data.status == "0") {
       $("#record_group_table").html(data.table);
