c4ac4d3721bf9f28efa755ef74b2c7639854ef61
shenlian <1019947054@qq.com>
2014-04-10 20:12:20 +0800

    add college name

--- adminStaff/utility.py
+++ adminStaff/utility.py
@@ -100,14 +100,7 @@ def info_xls_expertscore_gen():
     worksheet.write_merge(0, 0, 0, 10, '大连理工大学创新创业项目评分统计表',style)
 
     # generate body
-<<<<<<< HEAD
-    worksheet.write_merge(1, 1, 0, 0, '项目编号')
-    worksheet.col(0).width = len('项目编号') * 300
-    worksheet.write_merge(1, 1, 1, 1, '项目名称')
-    worksheet.col(1).width = len('项目名称') * 800
-    worksheet.write_merge(1, 1, 2, 2, '项目级别')
-    worksheet.write_merge(1, 1, 3, 3, '指导教师')
-=======
+
     worksheet.write_merge(1, 1, 0, 0, '项目申报编号')
     worksheet.col(0).width = len('项目申报编号') * 200
     worksheet.write_merge(1, 1, 1, 1, '名称')
@@ -116,7 +109,6 @@ def info_xls_expertscore_gen():
     worksheet.col(2).width = len('学院') * 800
     worksheet.write_merge(1, 1, 3, 3, '项目级别')
     worksheet.write_merge(1, 1, 4, 4, '指导教师')
->>>>>>> 9090199... add college name in export excel
     worksheet.col(3).width = len('指导教师') * 200
     worksheet.write_merge(1, 1, 5, 5, '项目选题意义')
     worksheet.write_merge(1, 1, 6, 6, '科技研究价值')
@@ -144,19 +136,6 @@ def info_xls_expertscore(request):
         scorelist=get_expertscore(proj_obj)
 
         row = 1 + _number
-<<<<<<< HEAD
-        xls_obj.write(row, 0, unicode(proj_obj.project_unique_code)) 
-        xls_obj.write(row, 1, unicode(proj_obj.title)) 
-        xls_obj.write(row, 2, unicode(proj_obj.project_grade)) 
-        xls_obj.write(row, 3, unicode(proj_obj.adminuser.get_name())) 
-        xls_obj.write(row, 4, unicode(scorelist[0])) 
-        xls_obj.write(row, 5, unicode(scorelist[1])) 
-        xls_obj.write(row, 6, unicode(scorelist[2]))
-        xls_obj.write(row, 7, unicode(scorelist[3])) 
-        xls_obj.write(row, 8, unicode(scorelist[4]))  
-        xls_obj.write(row, 9, unicode(scorelist[5])) 
-        xls_obj.write(row, 10, unicode(scorelist[6])) 
-=======
         xls_obj.write(row, 0, unicode(proj_obj.project_code)) 
         xls_obj.write(row, 1, unicode(proj_obj.title))
         xls_obj.write(row, 2, unicode(proj_obj.school.school))
@@ -169,7 +148,7 @@ def info_xls_expertscore(request):
         xls_obj.write(row, 9, unicode(scorelist[4]))  
         xls_obj.write(row, 10, unicode(scorelist[5])) 
         xls_obj.write(row, 11, unicode(scorelist[6])) 
->>>>>>> 9090199... add college name in export excel
+
 
 
         # _index += 1
@@ -232,7 +211,7 @@ def info_xls_summaryinnovate(request):
         print row_project_start
         xls_obj.write_merge(row_project_start,row,3,3,unicode(proj_obj.title),style)
         xls_obj.write_merge(row_project_start,row,4,4,unicode(proj_obj.adminuser.get_name()),style)
-        xls_obj.write_merge(row_project_start,row,5,5,unicode(proj_obj.adminuser.titles),style)
+        xls_obj.write_merge(row_project_start,row,5,5,unicode(proj_ob>j.adminuser.titles),style)
         xls_obj.write_merge(row_project_start,row,6,10)
         # _index += 1  
     # write xls file
