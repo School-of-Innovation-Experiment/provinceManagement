ea658238d4f9c942f1842a26392fde93d8267729
LiaoPengyu <ariselpy@gmail.com>
2014-04-11 17:32:46 +0800

    fix round

--- adminStaff/utility.py
+++ adminStaff/utility.py
@@ -184,7 +184,7 @@ def get_expertscore(proj_obj):
 
     cnt_of_list = len(review_list)
     # average_list = [sum(map(float, a)) / cnt_of_list for a in zip(*review_list)[1:]]
-    average_list = [sum(map(float, a)) / len(filter(bool, a)) if len(filter(bool, a)) else 0 for a in zip(*review_list)[1:]]
+    average_list = [round(sum(map(float, a)) / len(filter(bool, a)), 2) if len(filter(bool, a)) else 0 for a in zip(*review_list)[1:]]
     loginfo(p=average_list,label="average_list")
     return average_list
 
