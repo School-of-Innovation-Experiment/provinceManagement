1250ef42777e9f4f43f6c686d4200a9e13e7ffae
DutBright <757289247@qq.com>
2014-04-15 17:33:44 +0800

    add the expert application

--- a1.py
+++
@@ -1,9 +0,0 @@
-def test(news_list):
-    for new in news_list:
-        str_list = new.news_document.url.split('/')
-        for i in range(len(str_list)):
-            if str_list[i] == 'media':
-                str_list = str_list[i+1:]
-                break
-        new.news_document = str('/'.join(str_list))
-    return news_list
