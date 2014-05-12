3ca107fbcaab38bc46d9e562cdeb6f4a811b628d
DutBright <757289247@qq.com>
2014-05-12 20:15:42 +0800

    modify checkbrower

--- media/js/home/checkbrowser.js
+++ media/js/home/checkbrowser.js
@@ -1,17 +1,12 @@
-function getOs() 
-{ 
-	var checkerror=false;
-	if(isSafari=navigator.userAgent.indexOf("Safari")>0) { 
-        checkerror=true;
-	} 
-    if(isFirefox=navigator.userAgent.indexOf("Firefox")>0){  
-        checkerror=true;
-    } 
-    if(window.opera){  
-        checkerror=true;
-    } 
-	if (checkerror==false) {
-		$('#myModal').modal('show');
-	};			
-} 
+function getOs()
+{
+  var checkerror=false;
+  var u_agent = navigator.userAgent;
+  if(u_agent.indexOf("Safari")>-1 || u_agent.indexOf("Firefox")>-1 || window.opera){
+  }else if(u_agent.indexOf("MSIE 6.0")>-1 ||u_agent.indexOf("MSIE 7.0")>-1 ){
+    alert("为了达到最佳浏览效果，我们推荐您使用以下浏览器 请使用'谷歌浏览器'或'火狐浏览器'");
+  }else{
+    $('#myModal').modal('show');
+  }
+}
 window.onload = getOs;
