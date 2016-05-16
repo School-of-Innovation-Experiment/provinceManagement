60d35ac1f3c6aaabaf24bc6dee174bb8ac08bb23
David <youchen.du@gmail.com>
2016-05-16 11:09:08 +0800

    [update] update project code style in xls sheet.

--- adminStaff/utility.py
+++ adminStaff/utility.py
@@ -99,7 +99,8 @@ def info_xls(request):
         row = 4 + _number
         xls_obj.write(row, 0, "%s" % _format_number(_number))
         xls_obj.write(row, 1, unicode(proj_obj.school))
-        xls_obj.write(row, 2, unicode(proj_obj.project_code))
+        xls_obj.write(row, 2, unicode(proj_obj.project_code[:9]+proj_obj.project_code[-3:]))
+        #xls_obj.write(row, 2, unicode(proj_obj.project_code))
         xls_obj.write(row, 3, unicode(proj_obj.title))
         xls_obj.write(row, 4, unicode(proj_obj.financial_category))
         xls_obj.write(row, 5, unicode(proj_obj.project_category))
@@ -229,30 +230,36 @@ def scored_xls_gen():
     worksheet = workbook.add_sheet('sheet1')
     year = get_current_year()
 
-    worksheet.write_merge(0, 0, 0, 15, str(year)+"年辽宁省大学生创新创业训练计划评审得分汇总表")
-    worksheet.write_merge(1, 4, 0, 0, '序号')
-    worksheet.write_merge(1, 4, 1, 1, '学校')
-    worksheet.write_merge(1, 4, 2, 2, '项目编号')
-    worksheet.write_merge(1, 4, 3, 3, '项目名称')
-    worksheet.write_merge(1, 4, 4, 4, '项目类别（甲类、乙类）')
-    worksheet.write_merge(1, 4, 5, 5, '项目类型')
-    worksheet.write_merge(1, 4, 6, 6, '项目负责人')
-    worksheet.write_merge(1, 2, 7, 8, '指导教师')
-    worksheet.write_merge(3, 4, 7, 7, '姓名')
-    worksheet.write_merge(3, 4, 8, 8, '职称')
-    worksheet.write_merge(1, 4, 9, 9, '项目所属一级学科')
-    worksheet.write_merge(1, 4, 10, 10, '评分小组')
-    worksheet.write_merge(1, 2, 11, 13, '专家评分')
-    worksheet.write_merge(3, 4, 11, 11, '专家1')
-    worksheet.write_merge(3, 4, 12, 12, '专家2')
-    worksheet.write_merge(3, 4, 13, 13, '专家3')
-    worksheet.write_merge(1, 4, 14, 14, '平均分')
-    worksheet.write_merge(1, 4, 15, 15, '排名')
+    worksheet.write_merge(0, 0, 0, 23, str(year)+"年辽宁省大学生创新创业训练计划评审得分汇总表")
+    worksheet.write_merge(1, 4, 0, 0, '立项年份')
+    worksheet.write_merge(1, 4, 1, 1, '省（区、市）')
+    worksheet.write_merge(1, 4, 2, 2, '高校代码')
+    worksheet.write_merge(1, 4, 3, 3, '高校名称')
+    worksheet.write_merge(1, 4, 4, 4, '项目编号')
+    worksheet.write_merge(1, 4, 5, 5, '项目名称')
+    worksheet.write_merge(1, 4, 6, 6, '项目类型')
+    worksheet.write_merge(1, 4, 7, 7, '项目负责人姓名')
+    worksheet.write_merge(1, 4, 8, 8, '项目负责人学号')
+    worksheet.write_merge(1, 4, 9, 9, '参与学生人数')
+    worksheet.write_merge(1, 4, 10, 10, '项目其他成员信息')
+    worksheet.write_merge(1, 4, 11, 11, '指导教师姓名')
+    worksheet.write_merge(1, 4, 12, 12, '指导教师职称')
+    worksheet.write_merge(1, 4, 13, 13, '财政拨款（元）')
+    worksheet.write_merge(1, 4, 14, 14, '校拨（元）')
+    worksheet.write_merge(1, 4, 15, 15, '总经费（元）')
+    worksheet.write_merge(1, 4, 16, 16, '项目所属一级学科')
+    worksheet.write_merge(1, 4, 17, 17, '项目简介（200字以内）')
+    worksheet.write_merge(1, 4, 18, 18, '评分小组')
+    worksheet.write_merge(1, 2, 19, 21, '专家评分')
+    worksheet.write_merge(3, 4, 19, 19, '专家1')
+    worksheet.write_merge(3, 4, 20, 20, '专家2')
+    worksheet.write_merge(3, 4, 21, 21, '专家3')
+    worksheet.write_merge(1, 4, 22, 22, '平均分')
+    worksheet.write_merge(1, 4, 23, 23, '排名')
 
-    worksheet.col(1).width = len('学校') * 600
-    worksheet.col(2).width = len('项目编号') * 300
-    worksheet.col(3).width = len('项目名称') * 800
-    worksheet.col(4).width = len('项目类别（甲类、乙类）') * 64
+    worksheet.col(3).width = len('学校') * 600
+    worksheet.col(4).width = len('项目编号') * 300
+    worksheet.col(5).width = len('项目名称') * 800
 
     return worksheet, workbook
 
@@ -265,22 +272,37 @@ def scored_result_xls(request, sorted_list):
         for item_index, item in enumerate(group):
             rating = item_index+1
             proj, score1, score2, score3, total_score = item
-            xls_obj.write(row, 0, "%04d" % (row-4))
-            xls_obj.write(row, 1, unicode(proj.school))
-            xls_obj.write(row, 2, unicode(proj.school))
-            xls_obj.write(row, 3, unicode(proj.title))
-            xls_obj.write(row, 4, unicode(proj.financial_category))
-            xls_obj.write(row, 5, unicode(proj.project_category))
-            xls_obj.write(row, 6, unicode(proj.student.user.first_name))
-            xls_obj.write(row, 7, unicode(proj.inspector))
-            xls_obj.write(row, 8, unicode(proj.inspector_title))
-            xls_obj.write(row, 9, unicode(proj.insitute))
-            xls_obj.write(row, 10, unicode(group_index))
-            xls_obj.write(row, 11, unicode(score1))
-            xls_obj.write(row, 12, unicode(score2))
-            xls_obj.write(row, 13, unicode(score3))
-            xls_obj.write(row, 14, '%.3f' % (sum([score1, score2, score3])/3))
-            xls_obj.write(row, 15, unicode(rating))
+            school = proj.school.schoolprofile_set.all()[0]
+            students = proj.student_group_set.all()
+            charge_student = students[0]
+            members = students[1:]
+            presubmit_type = PreSubmit if proj.project_category.category == CATE_INNOVATION else PreSubmitEnterprise
+            presubmit = presubmit_type.objects.get(project_id=proj.project_id)
+            xls_obj.write(row, 0, unicode(proj.year)) 
+            xls_obj.write(row, 1, '辽宁省')
+            xls_obj.write(row, 2, unicode(school.userid))
+            xls_obj.write(row, 3, unicode(school.school))
+            xls_obj.write(row, 4, unicode(proj.project_code[:9]+proj.project_code[-3:]))
+            #xls_obj.write(row, 4, unicode(proj.project_code))
+            xls_obj.write(row, 5, unicode(proj.title))
+            xls_obj.write(row, 6, unicode(proj.project_category))
+            xls_obj.write(row, 7, unicode(charge_student.studentName))
+            xls_obj.write(row, 8, unicode(charge_student.studentId))
+            xls_obj.write(row, 9, unicode(students.count()))
+            xls_obj.write(row, 10, unicode(','.join(map(lambda x: x.studentName+'('+x.studentId+')', members))))
+            xls_obj.write(row, 11, unicode(proj.inspector))
+            xls_obj.write(row, 12, unicode(proj.inspector_title))
+            xls_obj.write(row, 13, '10000')
+            xls_obj.write(row, 14, '5000')
+            xls_obj.write(row, 15, '15000')
+            xls_obj.write(row, 16, unicode(proj.project_category))
+            xls_obj.write(row, 17, unicode(presubmit.proj_introduction))
+            xls_obj.write(row, 18, unicode(group_index))
+            xls_obj.write(row, 19, '%.3f' % score1)
+            xls_obj.write(row, 20, '%.3f' % score2)
+            xls_obj.write(row, 21, '%.3f' % score3)
+            xls_obj.write(row, 22, '%.3f' % (sum([score1, score2, score3])/3))
+            xls_obj.write(row, 23, unicode(rating))
             row += 1
     save_path = os.path.join(
         TMP_FILES_PATH,
