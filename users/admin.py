7656afdb80bf37cfb1c66189b98c6ab79d4811f0
David <youchen.du@gmail.com>
2016-12-05 11:21:21 +0800

    [add] search field in admin site for TeacherProfile.

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 # coding: UTF-8
+# Author: David
+# Email: youchen.du@gmail.com
+# Created: 2016-12-05 11:17
+# Last modified: 2016-12-05 11:21
+# Filename: admin.py
+# Description:
 '''
 Created on 2012-11-5
 
@@ -9,12 +15,17 @@ from django.contrib import admin
 from users.models import *
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 RegisterClass = (SchoolProfile,
                  ExpertProfile,
                  AdminStaffProfile,
-                 TeacherProfile,
                  StudentProfile,
                  )
 
 for item in RegisterClass:
     admin.site.register(item)
+
+admin.site.register(TeacherProfile, ProfileAdmin)
