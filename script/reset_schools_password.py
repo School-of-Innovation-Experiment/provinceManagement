39242dcaac8f06a7469716f4d247fcc748a007a7
David <youchen.du@gmail.com>
2017-05-11 19:09:38 +0800

    [update] Progress in reseting.

--- script/reset_schools_password.py
+++ script/reset_schools_password.py
@@ -3,7 +3,7 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-05-11 18:21
-# Last modified: 2017-05-11 18:33
+# Last modified: 2017-05-11 19:09
 # Filename: reset_schools_password.py
 # Description:
 # coding: UTF-8
@@ -18,16 +18,17 @@ import settings_dev
 
 setup_environ(settings_dev)
 
-from django.contrib.auth.models import User
 from const import *
 from users.models import SchoolProfile
 
 schools = SchoolProfile.objects.all()
 with open('school_passwords.txt', 'w') as f:
-    for school in schools:
+    for idx, school in enumerate(schools):
         school_name = school.school
         user = school.userid
         new_passwd = str(random.randrange(10000, 100000))
         user.set_password(new_passwd)
-        f.write('{} {} {}\n'.format(school_name, user.username, new_passwd))
         user.save()
+        record = '{} {} {}\n'.format(school_name, user.username, new_passwd)
+        print idx, record
+        f.write(record)
