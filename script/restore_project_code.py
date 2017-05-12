7f75527e2e06bf5974ee1b1e196f0a444d17d7f7
David <youchen.du@gmail.com>
2017-05-12 11:51:37 +0800

    [add] A script to restore project_code.

---
+++ script/restore_project_code.py
@@ -0,0 +1,45 @@
+#!python
+# coding: UTF-8
+# Author: David
+# Email: youchen.du@gmail.com
+# Created: 2017-05-11 18:21
+# Last modified: 2017-05-11 19:09
+# Filename: reset_schools_password.py
+# Description:
+# coding: UTF-8
+import sys
+import os
+
+sys.path.append(os.path.abspath('..'))
+
+import MySQLdb
+
+from django.core.management import setup_environ
+import settings_production
+
+setup_environ(settings_production)
+
+from const import *
+from school.models import *
+
+adminuser_id = '53'
+year = 2016
+
+
+conn = MySQLdb.connect(host='localhost', user='root', passwd='root',
+                       port=3306, db='tmpProvinceManagement')
+curs = conn.cursor()
+fmt = 'select project_id, project_code from school_projectsingle where year={} and adminuser_id={}'
+query = fmt.format(year, adminuser_id)
+curs.execute(query)
+id_codes = {}
+for project_id, project_code in curs.fetchall():
+    id_codes[project_id] = project_code
+
+projs = ProjectSingle.objects.filter(adminuser=adminuser_id).filter(year=year)
+for idx, proj in enumerate(projs, 1):
+    print idx, proj, proj.project_id, proj.project_code, id_codes[proj.project_id]
+    proj.project_code = id_codes[proj.project_id]
+    proj.save()
+
+
