b787341b688840342fe47b5622af4dce804622a9
David <youchen.du@gmail.com>
2017-05-15 15:42:34 +0800

    [update] check browser and update script.

--- script/restore_project_code.py
+++ script/restore_project_code.py
@@ -3,8 +3,8 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-05-11 18:21
-# Last modified: 2017-05-11 19:09
-# Filename: reset_schools_password.py
+# Last modified: 2017-05-15 15:38
+# Filename: restore_project_code.py
 # Description:
 # coding: UTF-8
 import sys
@@ -22,23 +22,23 @@ setup_environ(settings_production)
 from const import *
 from school.models import *
 
-adminuser_id = '53'
 year = 2016
-
 
 conn = MySQLdb.connect(host='localhost', user='root', passwd='root',
                        port=3306, db='tmpProvinceManagement')
 curs = conn.cursor()
-fmt = 'select project_id, project_code from school_projectsingle where year={} and adminuser_id={}'
-query = fmt.format(year, adminuser_id)
+fmt = 'select project_id, project_code from school_projectsingle where year={}'
+query = fmt.format(year)
 curs.execute(query)
 id_codes = {}
 for project_id, project_code in curs.fetchall():
     id_codes[project_id] = project_code
 
-projs = ProjectSingle.objects.filter(adminuser=adminuser_id).filter(year=year)
+projs = ProjectSingle.objects.filter(year=year)
+cnt = projs.count()
 for idx, proj in enumerate(projs, 1):
-    print idx, proj, proj.project_id, proj.project_code, id_codes[proj.project_id]
+    if idx % (cnt / 5) == 0:
+        print '{:4d} / {:4d}'.format(idx, cnt)
     proj.project_code = id_codes[proj.project_id]
     proj.save()
 
